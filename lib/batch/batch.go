34b018e015d6c62e178edc17aefb73d87280de76
anton_malashenko <anton.mal.slb@gmail.com>
2022-07-11 16:49:30 +0300

    add solution with goroutines

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -14,8 +14,21 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
+	ch := make(chan user)
+	sem := make(chan struct{}, pool)
+
 	for i := int64(0); i < n; i++ {
-		res = append(res, getOne(i))
+		go func(i int64) {
+			sem <- struct{}{}
+			ch <- getOne(i)
+			<-sem
+		}(i)
 	}
+
+	for i := int64(0); i < n; i++ {
+		user := <-ch
+		res = append(res, user)
+	}
+
 	return res
 }
